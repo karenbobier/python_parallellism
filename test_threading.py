992ba0bbf56a1c254ae8c9f0308a46647c368d11
Karen Bobier <karenbobier@gmail.com>
2024-10-02 12:08:02 -0400

    Add files via upload

---
+++ test_threading.py
@@ -0,0 +1,19 @@
+import threading
+
+import time
+
+from concurrent.futures import ThreadPoolExecutor
+
+import urllib.request as ur
+
+ 
+
+from Bio.Seq import Seq
+
+x = Seq("ATGGCGGGAAAATGA")
+
+x
+
+x[5]
+
+ 
